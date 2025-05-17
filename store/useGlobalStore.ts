06334762f490fb1dd2afd0b4fa44e42ed601514a
adiouf <h8799r@gmail.com>
2025-05-18 01:50:53 +0200

    Added page to select number of players

--- store/useGlobalStore.ts
+++ store/useGlobalStore.ts
@@ -7,13 +7,28 @@ export enum Mode {
 
 export interface GlobalAppData {
     mode?: Mode,
+    numberOfPlayers: number,
+    players: string[],
     setMode: (newMode: Mode) => void
+    resetMode: () => void
+    addPlayer: (player: string) => void
+    setNumberOfPlayers: (nbOfPlayers: number) => boolean
 }
 
 const useGlobalStore = create<GlobalAppData>((set) => ({
     mode: undefined,
-    setMode: (newMode: Mode) => set((state) => ({ mode: newMode })),
-    resetMdoe: () => set(state => ({mode: undefined}))
+    players: [],
+    numberOfPlayers: 1,
+    setMode: (newMode: Mode) => set(_state => ({ mode: newMode})),
+    resetMode: () => set(_state => ({ mode: undefined})),
+    addPlayer: (player: string) => set(state => ({players: [...state.players, player]})),
+    setNumberOfPlayers: (nbOfPlayers) => {
+        if(nbOfPlayers > 0) {
+            set(_state => ({ numberOfPlayers: nbOfPlayers }));
+            return true;
+        }
+        return false;
+    }
 }));
 
 export default useGlobalStore;
